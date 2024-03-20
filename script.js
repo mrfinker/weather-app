4b6c79c2c19e454f097d201ff617e072131ae347
mrfinker <caalebs@gmail.com>
2024-03-20 12:53:26 +0000

    20 mars 2024, 13:53

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container");
+const search = document.querySelector(".search-box button");
+const weatherBox = document.querySelector(".weather-box");
+const weatherDetails = document.querySelector(".weather-details");
+const error404 = document.querySelector(".not-found");
+
+search.addEventListener("click", () => {
+  
+  const apiKey = "2a69a01c3902cf1163c9597385f72284";
+  const city = document.querySelector(".search-box input").value;
+  if (city == "") return;
+  fetchWeatherData(city);
+});
