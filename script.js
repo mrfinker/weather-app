364e99ca8de546d833484ffcc5019eb8c7e333cc
mrfinker <caalebs@gmail.com>
2024-03-20 12:55:32 +0000

    20 mars 2024, 13:55

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const weatherDetails = document.querySelector(".weather-details");
 const error404 = document.querySelector(".not-found");
 
 search.addEventListener("click", () => {
-  
+
   const apiKey = "2a69a01c3902cf1163c9597385f72284";
   const city = document.querySelector(".search-box input").value;
   if (city == "") return;
-  fetchWeatherData(city);
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
 });
