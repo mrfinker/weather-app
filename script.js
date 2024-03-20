5311dcfe5766d7cac704d3d9b1d0b6131bd17a66
mrfinker <caalebs@gmail.com>
2024-03-20 13:01:53 +0000

    20 mars 2024, 14:01

--- script.js
+++ script.js
@@ -20,5 +20,48 @@ search.addEventListener("click", () => {
       error404.classList.add("fadeIn");
       return;
     }
+
+    error404.style.display = "none";
+    error404.classList.remove("fadeIn");
+
+    const image = document.querySelector(".weather-box img");
+    const temperature = document.querySelector(".weather-box .temperature");
+    const description = document.querySelector(".weather-box .description");
+    const humidity = document.querySelector(".weather-details .humidity span");
+    const wind = document.querySelector(".weather-details .wind span");
+
+    switch (json.weather[0].main) {
+      case "Clear":
+        image.src = "images/clear.png";
+        break;
+
+      case "Rain":
+        image.src = "images/rain.png";
+        break;
+
+      case "Snow":
+        image.src = "images/snow.png";
+        break;
+
+      case "Clouds":
+        image.src = "images/cloud.png";
+        break;
+
+      case "Haze":
+        image.src = "images/mist.png";
+        break;
+
+      default:
+        image.src = "";
+    }
+
+    temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+    description.innerHTML = `${json.weather[0].description}`;
+    humidity.innerHTML = `${json.main.humidity}%`;
+    wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+    weatherBox.style.display = "";
+    weatherDetails.style.display = "";
+    container.style.height = "590px";
+
   });
 });
