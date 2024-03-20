219bfd436a722a25280031240e2dbecf95efdb79
mrfinker <caalebs@gmail.com>
2024-03-20 12:57:49 +0000

    20 mars 2024, 13:57

--- script.js
+++ script.js
@@ -5,9 +5,20 @@ const weatherDetails = document.querySelector(".weather-details");
 const error404 = document.querySelector(".not-found");
 
 search.addEventListener("click", () => {
-
   const apiKey = "2a69a01c3902cf1163c9597385f72284";
   const city = document.querySelector(".search-box input").value;
   if (city == "") return;
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+  ).then((response) => response.json()).then((json) => {
+
+    if (json.cod == "404") {
+      container.style.height = "400px";
+      weatherBox.style.display = "none";
+      weatherDetails.style.display = "none";
+      error404.style.display = "block";
+      error404.classList.add("fadeIn");
+      return;
+    }
+  });
 });
